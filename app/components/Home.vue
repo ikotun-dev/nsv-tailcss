ce360011be94255f444fae2b2921dce75fed96ac
Ikotun Collins <danlogan2003@gmail.com>
2023-06-17 21:43:08 +0100

    some changes

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,18 +1,33 @@
 <template>
     <Page>
-        <ActionBar class="bg-blue-900">
-            <Label text="Home"/>
+        <ActionBar class="bg-blue-500">
+            <Label text="Home" class="text-white text-bold"/>
         </ActionBar>
 
-        <GridLayout>
-            <Label class="info">
-                <FormattedString>
-                    <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
-        </GridLayout>
-    </Page>
+        <GridLayout class="styled-div">
+      <Label text="Content goes here" class="text-lg" />
+    </GridLayout>
+
+          <StackLayout class="p-10 bg-gray-300">
+      <Label text="First Name" class="text-sm font-mono" />
+      <TextField v-model="firstName" class="input-field" />
+
+      <Label text="Last Name" class="text-lg" />
+      <TextField v-model="lastName" class="input-field" />
+
+      <Label text="Username" class="text-lg" />
+      <TextField v-model="username" class="input-field" />
+
+      <Label text="Password" class="text-lg" />
+      <TextField v-model="password" secure class="input-field" />
+
+      <Label text="Confirm Password" class="text-lg" />
+      <TextField v-model="confirmPassword" secure class="input-field" />
+
+      <Button text="Submit" @tap="submitForm" class="btn-primary mt-10" />
+    </StackLayout>
+
+          </Page>
 </template>
 
 <script>
@@ -38,4 +53,8 @@
         horizontal-align: center;
         vertical-align: center;
     }
+    .styled-div {
+  @apply border-2 border-black h-48 bg-gray-200 p-4;
+}
+
 </style>
